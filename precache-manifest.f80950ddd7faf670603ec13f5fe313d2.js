bad8fa17fc7a864d713a684d8db648b7bb97c23c
gaurav_daddyscode <gaurav.daddyscode@gmail.com>
2020-09-24 18:39:27 +0530

    Updates

---
+++ precache-manifest.f80950ddd7faf670603ec13f5fe313d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab43dc26144f9df136dba6a96161604b",
+    "url": "/react-expense-tracker/index.html"
+  },
+  {
+    "revision": "ee0b8db2303b25053b32",
+    "url": "/react-expense-tracker/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "52be1f5d5c695c93f168",
+    "url": "/react-expense-tracker/static/js/2.425c6326.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-expense-tracker/static/js/2.425c6326.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee0b8db2303b25053b32",
+    "url": "/react-expense-tracker/static/js/main.be36b382.chunk.js"
+  },
+  {
+    "revision": "4752dbf25a34323b5b7c",
+    "url": "/react-expense-tracker/static/js/runtime-main.38b51ddc.js"
+  }
+]);
